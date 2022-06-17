50b963a85dcd1fac99058ac5d76ea42da15cd5ea
Fat Cat Tony no sugar <88566085+58191554@users.noreply.github.com>
2022-06-17 11:15:14 +0800

    Add files via upload

---
+++ HM1/test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        int j = 3;
+        int k = j;
+        int i = k;
+        while(i<10){
+            i++;
+            System.out.println(i);
+        }
+    }
+}
